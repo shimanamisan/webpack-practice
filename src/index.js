964b27be8eca28c380125aca17f5792c9c49bacc
shimanamisan <mikan.sup.all@gmail.com>
2020-09-13 21:36:53 +0900

    uglifyjs-webpack-plugin

--- src/index.js
+++ src/index.js
@@ -2,4 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import './style.css'
 
+console.log('I am Log!')
+
 ReactDOM.render(<div class="react">Hi! React</div>, document.getElementById("root"));
