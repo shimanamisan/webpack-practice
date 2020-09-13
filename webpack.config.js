d00d1b497b351a554c49ee17625ac220ede5cf42
shimanamisan <mikan.sup.all@gmail.com>
2020-09-13 20:18:10 +0900

    webpack practice react

--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,9 @@ const path = require("path");
 // resolve()は、絶対パスを生成するメソッド
 // webpack.config.jsファイルが存在している現在のディレクトリ名と、distディレクトリを連結して絶対パスに変換している
 const outputPath = path.resolve(__dirname, "dist");
+//
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
 console.log("これがoutputPathです：" + outputPath);
 
 // モジュールにオブジェクトを設定する
@@ -42,15 +45,38 @@ module.exports = {
         // jpegとjpgの様にeがあるかないかを許容するのに、jpe?gという形式にする
         test: /\.(jpe?g|png|svg|gif|ico)$/i,
         loader: "url-loader",
+        // optionsを指定することで、file-loaderを有効化することが出来る
         options: {
           // 指定のサイズを超過すると、画像が[name]で指定されたファイルに書き換わり独立する
-          limit: 2048,
-          name: "./opimage/[name].[ext]",
+          // あたかもimagesディレクトリ配下に存在しているかのように、別ファイルとして出力できる（Developerツールでも確認済み）
+          limit: 1024, // サイズはKB
+          name: "./images/[name].[ext]",
         },
+      },
+      // babel-loaderを使用するための設定
+      {
+        // test: /\.m?js$/, // トランスパイルの対象の拡張子を指定
+        test: /\.jsx?$/,
+        exclude: /node_modules/, // node_module内のファイルはトランスパイルの対象とはしない。自分でコーディングしたファイルを対象とするため
+        loader: "babel-loader",
+      },
+      {
+        test: /\.html$/,
+        loader: "html-loader",
       },
     ],
   },
   devServer: {
     contentBase: outputPath,
   },
+  // プラグインを利用するための設定
+  plugins: [
+    // プラグインのインスタンスを生成
+    new HtmlWebpackPlugin({
+      // 雛形ファイルを指定する
+      template: './src/index.html', // 雛形ファイルが配置されている場所
+      filename: './index.html' // 出力するファイル名
+
+    })
+  ]
 };
